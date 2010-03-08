cb8cb01b4d9f1a498cc8a7495d8e663a1da14319
Trejkaz <trejkaz@trypticon.org>
2010-03-08 16:45:03 +1100

    Was accidentally putting (*.hex) into the description string twice.

--- main/src/main/org/trypticon/hex/gui/notebook/NotebookFileFilter.java
+++ main/src/main/org/trypticon/hex/gui/notebook/NotebookFileFilter.java
@@ -31,6 +31,6 @@ public class NotebookFileFilter extends FileExtensionFilter {
     }
 
     protected String getShortDescription() {
-        return "Hex Notebook Files (*.hex)";
+        return "Hex Notebook Files";
     }
 }
