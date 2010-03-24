3bc38423e04535f76e9db9061c56ee2a42314e9a
Trejkaz <trejkaz@bernkastel.trypticon.org>
2010-03-24 23:01:17 +1100

    Just adds one more annotation.

--- main/src/main/org/trypticon/hex/gui/sample/OpenSampleNotebookAction.java
+++ main/src/main/org/trypticon/hex/gui/sample/OpenSampleNotebookAction.java
@@ -1,6 +1,6 @@
 /*
  * Hex - a hex viewer and annotator
- * Copyright (C) 2009  Trejkaz, Hex Project
+ * Copyright (C) 2009-2010  Trejkaz, Hex Project
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -24,6 +24,8 @@ import org.trypticon.hex.anno.AnnotationCollection;
 import org.trypticon.hex.anno.SimpleMutableAnnotation;
 import org.trypticon.hex.anno.nulls.NullInterpretor;
 import org.trypticon.hex.anno.primitive.UIntInterpretorBE;
+import org.trypticon.hex.anno.primitive.UShortInterpretorBE;
+import org.trypticon.hex.anno.strings.StringInterpretor;
 import org.trypticon.hex.binary.BinaryUtils;
 import org.trypticon.hex.gui.HexFrame;
 import org.trypticon.hex.gui.notebook.Notebook;
@@ -55,8 +57,11 @@ public class OpenSampleNotebookAction extends BaseAction {
                                 0x2C, 0x20, 0x68, 0x61, 0x63, 0x6B, 0x69, 0x6E, 0x67, 0x20, 0x79, 0x6F,
                                 0x75, 0x72, 0x20, 0x62, 0x79, 0x74, 0x65, 0x73, 0x2E };
         long hiddenStringPos = BinaryUtils.positionOf(notebook.getBinary(), hiddenString);
+        annotations.add(new SimpleMutableAnnotation(hiddenStringPos - 2, 2,
+                                                    new UShortInterpretorBE(),
+                                                    null));
         annotations.add(new SimpleMutableAnnotation(hiddenStringPos, hiddenString.length,
-                                                    new NullInterpretor(),
+                                                    new StringInterpretor("UTF-8"),
                                                     null));
     }
 }
