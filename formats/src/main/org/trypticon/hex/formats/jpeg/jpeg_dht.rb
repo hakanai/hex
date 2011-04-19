bf8f88c493cc7c410666f25e0adb455b70f732fb
Trejkaz (pen name) <trejkaz@trypticon.org>
2011-04-19 20:45:43 +1000

    DHT support for JPEG, although less than great.

---
+++ formats/src/main/org/trypticon/hex/formats/jpeg/jpeg_dht.rb
@@ -0,0 +1,42 @@
+
+# DHT = Define Huffman Table
+
+structure :huffman_table do
+  # bit 0..3   number of tables
+  # bit 4      type of table  0=DC 1=AC
+  # bit 5..7   reserved =0
+  uint8      :huffman_table_info
+
+  # TODO: A way to represent this as an array and still be able to derive the value below.
+
+  uint8      :length1_count
+  uint8      :length2_count
+  uint8      :length3_count
+  uint8      :length4_count
+  uint8      :length5_count
+  uint8      :length6_count
+  uint8      :length7_count
+  uint8      :length8_count
+  uint8      :length9_count
+  uint8      :length10_count
+  uint8      :length11_count
+  uint8      :length12_count
+  uint8      :length13_count
+  uint8      :length14_count
+  uint8      :length15_count
+  uint8      :length16_count
+
+  unknown    :table_data, :length => ':length1_count + :length2_count + :length3_count + :length4_count +
+                                      :length5_count + :length6_count + :length7_count + :length8_count +
+                                      :length9_count + :length10_count + :length11_count + :length12_count +
+                                      :length13_count + :length14_count + :length15_count + :length16_count'
+end
+
+
+structure :dht do
+  uint16_be  :blockid      # Always FF C4
+  uint16_be  :length
+
+  # Length value includes the length we already read.
+  array      :tables, :size_type => :fixed_byte_size, :byte_size => ':length - 2', :element_type => :huffman_table
+end
--- formats/src/main/org/trypticon/hex/formats/jpeg/jpeg_image.rb
+++ formats/src/main/org/trypticon/hex/formats/jpeg/jpeg_image.rb
@@ -1,5 +1,6 @@
 
 require 'org/trypticon/hex/formats/jpeg/jpeg_sof'
+require 'org/trypticon/hex/formats/jpeg/jpeg_dht'
 require 'org/trypticon/hex/formats/jpeg/jpeg_soi'
 require 'org/trypticon/hex/formats/jpeg/jpeg_eoi'
 require 'org/trypticon/hex/formats/jpeg/jpeg_sos'
@@ -11,6 +12,7 @@ structure :jpeg_block do
   switch  :block_id, :replaces_this_structure => true do |value|
     case value
       when 0xFFC0 ; :sof0
+      when 0xFFC4 ; :dht
       when 0xFFD8 ; :soi
       when 0xFFD9 ; :eoi
       when 0xFFDA ; :sos
