acb6297401932ca4abffd0bc047efe2e26e14601
Trejkaz <trejkaz@trypticon.org>
2014-06-01 22:27:57 +1000

    JRuby doesn't seem to import 'java' by default anymore. Mysteriously this script works fine in the real application, but standalone it breaks.

--- formats/src/main/org/trypticon/hex/formats/ruby/structure_dsl.rb
+++ formats/src/main/org/trypticon/hex/formats/ruby/structure_dsl.rb
@@ -16,6 +16,8 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+require 'java'
+
 include_class org.trypticon.hex.anno.SimpleMutableAnnotation
 include_class org.trypticon.hex.anno.SimpleMutableGroupAnnotation
 include_class org.trypticon.hex.interpreters.FixedLengthInterpreter
