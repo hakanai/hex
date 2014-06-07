55d77eb84f05011eb57071f5ad9526e432e5c330
Trejkaz <trejkaz@trypticon.org>
2014-06-07 16:50:47 +1000

    Making the colour scheme a bit more complex.

--- main/src/main/org/trypticon/hex/gui/anno/CustomAnnotationStyleScheme.java
+++ main/src/main/org/trypticon/hex/gui/anno/CustomAnnotationStyleScheme.java
@@ -18,9 +18,36 @@
 
 package org.trypticon.hex.gui.anno;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Stroke;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jetbrains.annotations.Nullable;
+
 import org.trypticon.hex.AnnotationStyle;
 import org.trypticon.hex.DefaultAnnotationStyleScheme;
 import org.trypticon.hex.anno.Annotation;
+import org.trypticon.hex.anno.GroupAnnotation;
+import org.trypticon.hex.interpreters.Value;
+import org.trypticon.hex.interpreters.dates.Date;
+import org.trypticon.hex.interpreters.dates.DateTime;
+import org.trypticon.hex.interpreters.dates.Time;
+import org.trypticon.hex.interpreters.nulls.NullValue;
+import org.trypticon.hex.interpreters.primitives.floating.Float128;
+import org.trypticon.hex.interpreters.primitives.floating.Float16;
+import org.trypticon.hex.interpreters.primitives.floating.Float32;
+import org.trypticon.hex.interpreters.primitives.floating.Float64;
+import org.trypticon.hex.interpreters.primitives.signed.SByte;
+import org.trypticon.hex.interpreters.primitives.signed.SInt;
+import org.trypticon.hex.interpreters.primitives.signed.SLong;
+import org.trypticon.hex.interpreters.primitives.signed.SShort;
+import org.trypticon.hex.interpreters.primitives.unsigned.UByte;
+import org.trypticon.hex.interpreters.primitives.unsigned.UInt;
+import org.trypticon.hex.interpreters.primitives.unsigned.ULong;
+import org.trypticon.hex.interpreters.primitives.unsigned.UShort;
+import org.trypticon.hex.interpreters.strings.StringValue;
 
 /**
  * Customised annotation style scheme to allow for more style customisation.
@@ -28,6 +55,51 @@ import org.trypticon.hex.anno.Annotation;
  * @author trejkaz
  */
 public class CustomAnnotationStyleScheme extends DefaultAnnotationStyleScheme {
+    private final Map<Class<? extends Value>, AnnotationStyle> styleMap = new HashMap<>();
+
+    public CustomAnnotationStyleScheme() {
+        Stroke basicStroke = new BasicStroke(1.0f);
+
+        // Unknown values
+        styleMap.put(NullValue.class, new AnnotationStyle(
+            basicStroke, new Color(204, 204, 204).darker(), new Color(204, 204, 204)));
+
+        // Signed integers
+        AnnotationStyle signedStyle = new AnnotationStyle(
+            basicStroke, new Color(153, 255, 255).darker(), new Color(153, 255, 255));
+        styleMap.put(SByte.class, signedStyle);
+        styleMap.put(SShort.class, signedStyle);
+        styleMap.put(SInt.class, signedStyle);
+        styleMap.put(SLong.class, signedStyle);
+
+        // Unsigned integers
+        AnnotationStyle unsignedStyle = new AnnotationStyle(
+            basicStroke, new Color(153, 255, 153).darker(), new Color(153, 255, 153));
+        styleMap.put(UByte.class, unsignedStyle);
+        styleMap.put(UShort.class, unsignedStyle);
+        styleMap.put(UInt.class, unsignedStyle);
+        styleMap.put(ULong.class, unsignedStyle);
+
+        // Floating point
+        AnnotationStyle floatStyle = new AnnotationStyle(
+            basicStroke, new Color(255, 153, 255).darker(), new Color(255, 153, 255));
+        styleMap.put(Float16.class, floatStyle);
+        styleMap.put(Float32.class, floatStyle);
+        styleMap.put(Float64.class, floatStyle);
+        styleMap.put(Float128.class, floatStyle);
+
+        // Dates and times
+        AnnotationStyle dateStyle = new AnnotationStyle(
+            basicStroke, new Color(255, 153, 153).darker(), new Color(255, 153, 153));
+        styleMap.put(Date.class, dateStyle);
+        styleMap.put(Time.class, dateStyle);
+        styleMap.put(DateTime.class, dateStyle);
+
+        // Strings
+        styleMap.put(StringValue.class, new AnnotationStyle(
+            basicStroke, new Color(255, 255, 153).darker(), new Color(255, 255, 153)));
+    }
+
     @Override
     public AnnotationStyle getStyle(Annotation annotation) {
         if (annotation instanceof ExtendedAnnotation) {
@@ -37,8 +109,53 @@ public class CustomAnnotationStyleScheme extends DefaultAnnotationStyleScheme {
             }
         }
 
-        //TODO: A configurable colour scheme based on the interpreter type would be nice too.
+        if (annotation instanceof GroupAnnotation) {
+            return super.getStyle(annotation);
+        } else {
+            Class<? extends Value> type = annotation.getInterpreter().getType();
+            AnnotationStyle style = styleMap.get(type);
+            if (style == null) {
+                style = computeStyle(type);
+                if (style == null) {
+                    style = super.getStyle(annotation); // last resort
+                }
+                styleMap.put(type, style);
+            }
+            return style;
+        }
+    }
 
-        return super.getStyle(annotation);
+    /**
+     * Slower method to compute the type for a given value class by searching all superclasses
+     * and interfaces.
+     *
+     * @param type the type.
+     * @return the computed style or {@code null} if it could not be computed.
+     */
+    @Nullable
+    private AnnotationStyle computeStyle(Class<? extends Value> type) {
+        AnnotationStyle style = styleMap.get(type);
+        if (style != null) {
+            return style;
+        }
+
+        Class<?> superclass = type.getSuperclass();
+        if (Value.class.isAssignableFrom(superclass)) {
+            style = computeStyle(superclass.asSubclass(Value.class));
+            if (style != null) {
+                return style;
+            }
+        }
+
+        for (Class<?> face : type.getInterfaces()) {
+            if (Value.class.isAssignableFrom(face)) {
+                style = computeStyle(superclass.asSubclass(Value.class));
+                if (style != null) {
+                    return style;
+                }
+            }
+        }
+
+        return null;
     }
 }
